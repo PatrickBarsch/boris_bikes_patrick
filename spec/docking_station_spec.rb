8c5d5053593510fbe930d0b77b8f9045126c8a52
Patrick Barsch <PatrickGB@gmx.de>
2021-10-14 21:54:50 +0200

    Using doubles - tests are failing

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,8 @@ require 'bike'
 
 describe DockingStation do 
 
+  let (:bike) {double(:bike)}
+
   it "will recognise 'release_bike'" do
     expect(subject).to respond_to(:release_bike)  
   end
@@ -11,22 +13,20 @@ describe DockingStation do
   it "returns a bike, when release_bike is called" do
 
     docking_station = DockingStation.new
-    new_bike = Bike.new 
+    new_bike = bike
     docking_station.dock(new_bike)
-    expected = Bike
-    actual = (docking_station.release_bike).class
-    expect(actual).to be expected 
+    expect(docking_station.release_bike).to eq new_bike
 
   end
 
   it "returns a bike, which is working" do
-    subject.dock(Bike.new)
+    subject.dock(new_bike)
     expect(subject.release_bike).to be_working
 
   end
   
   it 'checks that a bike that is put in is properly released again' do
-    new_bike = Bike.new
+    new_bike = new_bike
     subject.dock(new_bike)
     expect(subject.release_bike).to be new_bike
   end 
@@ -37,24 +37,24 @@ describe DockingStation do
 
   it 'can store up to default capacity bikes' do 
     expect {DockingStation::DEFAULT_CAPACITY.times do
-      subject.dock(Bike.new)
+      subject.dock(bike)
       end}.not_to raise_error
   end
 
   it 'does not accept any new bikes once capcaity is reached' do
-    DockingStation::DEFAULT_CAPACITY.times {subject.dock(Bike.new)}
-    expect{subject.dock(Bike.new)}.to raise_error("Already at capacity")
+    DockingStation::DEFAULT_CAPACITY.times {subject.dock(bike)}
+    expect{subject.dock(bike)}.to raise_error("Already at capacity")
   end 
 
   it 'allows us to set a capacity' do
     capacity = 10
     station = DockingStation.new(capacity)
-    expect{capacity.times{station.dock(Bike.new)}}.not_to raise_error
-    expect{station.dock(Bike.new)}.to raise_error("Already at capacity")
+    expect{capacity.times{station.dock(bike)}}.not_to raise_error
+    expect{station.dock(bike)}.to raise_error("Already at capacity")
   end
 
   it 'does not release a broken bike' do
-    bike = Bike.new
+    bike = new_bike
     bike.report_broken
     subject.dock(bike)
     expect{subject.release_bike}.to raise_error("NoBikeError")
@@ -62,7 +62,7 @@ describe DockingStation do
 
   it 'finds and releases only working bikes' do
     3.times do |i|
-      bike = Bike.new
+      bike = new_bike
       bike.report_broken if i.even?
       subject.dock(bike)
     end
